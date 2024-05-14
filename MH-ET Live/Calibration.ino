4995bdf15216af04cc9f2f7e1e87006c8a7cbd62
r57zone <r57zone@gmail.com>
2024-05-14 21:14:31 +0400

    Added third pedal

--- MH-ET Live/Calibration.ino
+++ MH-ET Live/Calibration.ino
@@ -3,16 +3,21 @@
 // Setup
 int Pedal_1_Analog_Pin = 1;
 int Pedal_2_Analog_Pin = 0;
+//int Pedal_3_Analog_Pin = 0;
 
 
 int AnalogValue1 = 0;
 int AnalogValue2 = 0;
+//int AnalogValue3 = 0;
 
 int AnalogValue1Min = 1024;
 int AnalogValue1Max = 0;
 
 int AnalogValue2Min = 1024;
 int AnalogValue2Max = 0;
+
+//int AnalogValue3Min = 1024;
+//int AnalogValue3Max = 0;
 
 void setup()
 {  
@@ -23,11 +28,16 @@ void loop()
 {
 	AnalogValue1 = analogRead(Pedal_1_Analog_Pin);
 	AnalogValue2 = analogRead(Pedal_2_Analog_Pin);
+	//AnalogValue3 = analogRead(Pedal_3_Analog_Pin);
 
 	DigiKeyboard.sendKeyStroke(0);
 
-	DigiKeyboard.println("Pedal1 min=" + String(AnalogValue1Min) + " max=" + String(AnalogValue1Max) + " pedal 2 min=" + 
+	DigiKeyboard.println("Pedal1 min=" + String(AnalogValue1Min) + " max=" + String(AnalogValue1Max) + " Pedal2 min=" + 
 						  String(AnalogValue2Min) + " max=" + String(AnalogValue2Max));
+	
+	//DigiKeyboard.println("Pedal1 min=" + String(AnalogValue1Min) + " max=" + String(AnalogValue1Max) + " Pedal2 min=" + 
+	//					  String(AnalogValue2Min) + " max=" + String(AnalogValue2Max) + " Pedal3 min=" + 
+	//					  String(AnalogValue2Min) + " max=" + String(AnalogValue2Max));
 
 	if (AnalogValue1Min > AnalogValue1)
 		AnalogValue1Min = AnalogValue1;
@@ -38,6 +48,11 @@ void loop()
 		AnalogValue2Min = AnalogValue2;
 	if (AnalogValue2Max < AnalogValue2)
 		AnalogValue2Max = AnalogValue2;
+	
+	//if (AnalogValue3Min > AnalogValue3)
+		//AnalogValue3Min = AnalogValue3;
+	//if (AnalogValue3Max < AnalogValue3)
+		//AnalogValue3Max = AnalogValue3;
 
 	delay(1000);
 }
--- MH-ET Live/Firmware.ino
+++ MH-ET Live/Firmware.ino
@@ -3,6 +3,7 @@
 // Setup
 int Pedal_1_Analog_Pin = 1;
 int Pedal_2_Analog_Pin = 0;
+//int Pedal_3_Analog_Pin = 0;
 
 byte buttons = 0x00;
 
@@ -16,22 +17,33 @@ int Pedal2Min = 188;
 int Pedal2Max = 895;
 int DeadZonePercentPedal2 = 2;
 
+//int Pedal3Min = 188;
+//int Pedal3Max = 895;
+//int DeadZonePercentPedal3 = 2;
+
 // Sketch
 float PedalsValues[2];
+//float PedalsValues[3];
 
 int Pedal1Diff = (Pedal1Max - Pedal1Min) / 100.0;
 int Pedal1DiffFull = (Pedal1Max - Pedal1Min);
 int Pedal2Diff = (Pedal2Max - Pedal2Min) / 100.0;
 int Pedal2DiffFull = (Pedal2Max - Pedal2Min);
+//int Pedal3Diff = (Pedal3Max - Pedal3Min) / 100.0;
+//int Pedal3DiffFull = (Pedal3Max - Pedal3Min);
 
 int AnalogValue1 = 0;
 int AnalogValue2 = 0;
+//int AnalogValue3 = 0;
 
 int AnalogValue1Min = 1024;
 int AnalogValue1Max = 0;
 
 int AnalogValue2Min = 1024;
 int AnalogValue2Max = 0;
+
+//int AnalogValue3Min = 1024;
+//int AnalogValue3Max = 0;
 
 float DeadZoneAxis(float StickAxis, float DeadZone)
 {
@@ -48,18 +60,23 @@ void setup()
 	DigiJoystick.setX((byte)127);
 	DigiJoystick.setY((byte)127);
 	DigiJoystick.setZROT((byte)0);
+	DigiJoystick.setXROT((byte)0);
+	DigiJoystick.setYROT((byte)0);
 }
 
 void loop()
 {
 	AnalogValue1 = analogRead(Pedal_1_Analog_Pin);
 	AnalogValue2 = analogRead(Pedal_2_Analog_Pin);
+	//AnalogValue3 = analogRead(Pedal_3_Analog_Pin);
 
 	PedalsValues[0] = 1.0 - DeadZoneAxis((AnalogValue1 - Pedal1Min) / Pedal1Diff, DeadZonePercentPedal1);
 	PedalsValues[1] = 1.0 - DeadZoneAxis((AnalogValue2 - Pedal2Min) / Pedal2Diff, DeadZonePercentPedal2);
+	//PedalsValues[2] = 1.0 - DeadZoneAxis((AnalogValue3 - Pedal3Min) / Pedal3Diff, DeadZonePercentPedal3);
 
 	DigiJoystick.setXROT((byte)(PedalsValues[0] * 255));
 	DigiJoystick.setYROT((byte)(PedalsValues[1] * 255));
+	//DigiJoystick.setZROT((byte)(PedalsValues[2] * 255));
 
 	buttons = 0x00;
 
@@ -67,6 +84,8 @@ void loop()
 		buttons |= 0x1;
 	if (PedalsValues[1] > 0.2)
 		buttons |= 0x2;
+	//if (PedalsValues[2] > 0.2)
+		//buttons |= 0x4;
 
 	DigiJoystick.setButtons((byte)buttons, 0x00);
 	DigiJoystick.update();    
